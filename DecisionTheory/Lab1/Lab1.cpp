6766a2e959fe0b6fbef46668e8663e7d1924e764
Dudyak Michael <sergolwow@gmail.com>
2018-06-24 23:05:35 +0200

    Initial commit for Decision Theory

---
+++ DecisionTheory/Lab1/Lab1.cpp
@@ -0,0 +1,16 @@
+// Lab1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+	cout << "Well, hello world, once again!" << endl;
+	_getch();
+    return 0;
+}
+
